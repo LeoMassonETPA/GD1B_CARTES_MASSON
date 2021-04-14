f64a63ca9fc82e0fdeb6d3928941e41090ae64d9
LeoMassonETPA <l.masson@rns.ecolescreatives.com>
2021-04-14 14:40:34 +0200

    CARTES.cpp

--- Monstre.cpp
+++ Monstre.cpp
@@ -2,3 +2,29 @@
 #include <string>
 #include "Monstre.h"
 
+ /*private:
+
+        char _nom;
+        int _attaque;
+        int _pdV;
+        bool _epuise;
+
+
+    public:
+
+        void AtqMonstre();
+        void MortMonstre();
+        void AfficheInfo(); */
+
+void Monstre::AtqMonstre(){
+
+}
+
+void Monstre::MortMonstre(){
+
+}
+
+void Monstre::AfficheInfo(){
+
+}
+
--- Monstre.h
+++ Monstre.h
@@ -1,2 +1,21 @@
 #include <iostream>
 #include <string>
+
+class Monstre{
+
+    private:
+
+        char _nom;
+        int _attaque;
+        int _pdV;
+        bool _epuise;
+
+
+    public:
+
+        void AtqMonstre();
+        void MortMonstre();
+        void AfficheInfo();
+
+        Monstre(char nom, int attaque, int pdV);
+};
