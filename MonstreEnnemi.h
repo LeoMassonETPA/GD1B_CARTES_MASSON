4c93ff142333aec59cadf7b38383f89180d40590
LeoMassonETPA <l.masson@rns.ecolescreatives.com>
2021-04-14 16:14:33 +0200

    Update MonstreEnnemi.h

--- MonstreEnnemi.h
+++ MonstreEnnemi.h
@@ -1,1 +1,22 @@
-MonstreEnnemi.h
+#include <iostream>
+#include <string>
+
+
+class MonstreEnnemi{
+
+    private:
+
+        char _nomEnnemi;
+        int _attaqueEnnemi;
+        int _pdVEnnemi;
+        bool _epuiseEnnemi;
+        bool _mortEnnemi;
+
+    public:
+
+        void AtqMonstreEnnemi();
+        void MortMonstreEnnemi();
+        void AfficheInfoEnnemi();
+
+        MonstreEnnemi(char nomEnnemi, int attaqueEnnemi, int pdVEnnemi);
+};
